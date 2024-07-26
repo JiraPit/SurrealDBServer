eb14998612eca478fd03340633c95910880c3165
Jira Pit <pitakwongjira@gmail.com>
2024-07-26 17:58:05 +0700

    remove embedded router

---
+++ surrealdb_rpc_service/dockerbuild.sh
@@ -0,0 +1,2 @@
+cp ../proto/authentication_service.proto "proto(copied)/authentication_service.proto"
+docker build . -t surreal_rpc_service
--- surrealdb_rpc_service/src/main.rs
+++ surrealdb_rpc_service/src/main.rs
@@ -5,17 +5,7 @@ mod surreal_server;
 use authentication_service::{
     authentication_service_server::AuthenticationServiceServer, AuthenticationService,
 };
-use axum::{
-    body::Body,
-    http::{Request, Response, StatusCode},
-    routing::any,
-    Router,
-};
-use hyper::client::conn::http1;
-use hyper::client::conn::http2;
-use hyper_tls::HttpsConnector;
-use std::{env, error::Error};
-use tokio::time::{sleep, Duration};
+use std::env;
 use tonic::transport::Server;
 
 #[tokio::main]
@@ -30,7 +20,7 @@ async fn main() {
     }
 
     // Start a Surreal server
-    let surreal_handler = match surreal_server::start().await {
+    let _surreal_handler = match surreal_server::start().await {
         Ok(handler) => handler,
         Err(e) => {
             log::error!("Failed to start the Surreal server: {}", e);
@@ -38,69 +28,24 @@ async fn main() {
         }
     };
 
-    // Start an authentication service on a separate thread
-    let auth_service_handler = match spawn_auth_service().await {
-        Ok(handler) => handler,
-        Err(e) => {
-            log::error!("Failed to start the authentication service: {}", e);
-            return;
-        }
-    };
-
-    // Start the router and the listener
-    let router = Router::new()
-        .route("/interface", any(handle_interface_route))
-        .route("/data", any(handle_data_route));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
-        .await
-        .expect("Error binding server");
-
-    // server the server
-    axum::serve(listener, router)
-        .await
-        .expect("Error running server");
-
-    // Explicitly stop the services and drop the handlers
-    auth_service_handler.abort();
-    while !auth_service_handler.is_finished() {
-        sleep(Duration::from_millis(300)).await;
-    }
-    drop(surreal_handler);
-    drop(auth_service_handler);
-}
-
-/// Spawns an authentication service on a separate thread
-async fn spawn_auth_service() -> Result<tokio::task::JoinHandle<()>, Box<dyn Error>> {
     // Create a new authentication service
     let mut auth_service = AuthenticationService::new("test", "auth");
-    auth_service.start().await?;
+    if let Err(e) = auth_service.start().await {
+        log::error!("Failed to start the authentication service: {}", e);
+        return;
+    }
 
     // Get the RPC address
     let rpc_port = env::var("RPC_PORT").expect("RPC_PORT is not set");
     let addr = format!("0.0.0.0:{rpc_port}").parse().unwrap();
 
     // Start the authentication service on a separate thread
-    let handler = tokio::spawn(async move {
-        // Start the authentication service
-        log::info!("Server starting at: {}", addr);
-        if let Err(e) = Server::builder()
-            .add_service(AuthenticationServiceServer::new(auth_service))
-            .serve(addr)
-            .await
-        {
-            log::error!("Failed to start auth service: {}", e);
-        }
-    });
-
-    Ok(handler)
-}
-
-/// Handles the interface route
-async fn handle_interface_route(req: Request<Body>) -> Result<Response<Body>, StatusCode> {
-    todo!()
-}
-
-/// Handles the data route
-async fn handle_data_route(req: Request<Body>) -> Result<Response<Body>, StatusCode> {
-    todo!()
+    log::info!("Server starting at: {}", addr);
+    if let Err(e) = Server::builder()
+        .add_service(AuthenticationServiceServer::new(auth_service))
+        .serve(addr)
+        .await
+    {
+        log::error!("Failed to start auth service: {}", e);
+    };
 }
